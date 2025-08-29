39dac92df9ac04302918e388a9b571d14730158f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-08-29 21:02:32 +0530

    Update Project.js

--- server/models/Project.js
+++ server/models/Project.js
@@ -3,20 +3,37 @@ const mongoose = require('mongoose');
 const ProjectSchema = new mongoose.Schema({
     pname: {
         type: String,
-        trim: true,
         required: true,
-        unique: true,
+        unique: true
     },
-    desc: {
+    pdescription: {
         type: String,
         required: true,
+        unique: true
     },
-    github_url: {
+    giturl: {
         type: String,
-        required: true,        
+        required: true,
+        unique: true
     },
-    
-});
+    pmembers: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    psupervisor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    pstartdate: {
+        type: Date,
+        required: true,
+        default: new Date()
+    },
+    estimatedEndDate: {
+        type: Date,
+        required: true,
+    }
+}, { timestamps: true });
 
 const Project = mongoose.model('Project', ProjectSchema);
 
