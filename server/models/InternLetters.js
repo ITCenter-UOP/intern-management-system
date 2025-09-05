dc655bddea797d45239ff1f18ff119f70cae5658
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-09-05 09:39:36 +0530

    Create InternLetters.js

---
+++ server/models/InternLetters.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const InternLettersSchema = new mongoose.Schema({
+    userID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    letter_type: {
+        type: String,
+        enum: ['internship_start_letter', 'letter_of_internship'],
+        required: true
+    },
+    letter_infor: [
+        {
+            issue_at: { type: Date },
+            start_at: { type: Date },
+            end_at: { type: Date },
+            supervisor: { type: String }
+        }
+    ]
+}, { timestamps: true });
+
+const InternLetters = mongoose.model('InternLetters', InternLettersSchema);
+
+module.exports = InternLetters;
