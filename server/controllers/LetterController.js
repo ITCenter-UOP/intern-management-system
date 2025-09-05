0ea2925902157b4252f3e3e59bb578b0c06ed6dd
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-09-05 09:42:15 +0530

    Create LetterController.js

---
+++ server/controllers/LetterController.js
@@ -0,0 +1,12 @@
+const LetterController = {
+    create_internship_start_letter: async (req, res) => {
+        try {
+            
+        }
+        catch (error) {
+            console.log(err)
+        }
+    }
+};
+
+module.exports = LetterController;
